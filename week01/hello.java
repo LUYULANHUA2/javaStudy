d40faab15387d9e4846581c153a364e585bc8635
WangNan <WangNan@git.com>
2024-04-23 20:04:22 +0800

    00

--- week01/hello.java
+++ week01/hello.java
@@ -1,7 +1,13 @@
 package week01;
 
+import java.util.Scanner;
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+
+        int num01 = sc.nextInt();
+        System.out.println(num01);
     }
 }
