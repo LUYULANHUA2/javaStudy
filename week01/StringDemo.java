8a2c2a0d18de99a9f5d3b8fd60e15a919c12d96a
WangNan <WangNan@git.com>
2024-05-05 17:43:18 +0800

    main

--- week01/StringDemo.java
+++ week01/StringDemo.java
@@ -5,6 +5,7 @@ import java.util.StringJoiner;
 public class StringDemo {
 
     public static void main(String[] args) {
+        //StringJoiner的基本使用：
         StringJoiner sj = new StringJoiner("--", "[", "]");
         sj.add("abd");
         sj.add("555");
@@ -19,6 +20,7 @@ public class StringDemo {
         System.out.println(ch);
 
         // stringbuilder:
+        /*stringBuilder转换成String的过程 */
         StringBuilder sb = new StringBuilder();
         sb.append("str");
         sb.append(true);
@@ -26,12 +28,25 @@ public class StringDemo {
         System.out.println(sb);
         String str2 = sb.toString();
         System.out.println(str2);
+
+        //访问String中具体位置的方法
         char c = str2.charAt(3);
         System.out.println(c);
 
+        //两个在串池中公用一个"abc"
         String s1 = "abc";
         String s2 = "a" + "b" + "c";
-        System.out.println("s1 = s2 ?"+s1 == s2);
+        System.out.println(s1 == s2);
+
+        //StringBUilder的底层实现，当超过基础的容量，
+        //进行扩容*2+2，超过扩容容量的时候，以实际的长度为容量
+        StringBuilder sb1 = new StringBuilder();
+        System.out.println(sb1.capacity());
+        System.out.println(sb1.length());
+        sb1.append("abcdefgabcdefgabcdefgabcdefgabcdefg");
+        System.out.println(sb1.capacity());
+        System.out.println(sb1.length());
+
 
     }
 }
