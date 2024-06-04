d2eb40147b4b5099959bc6ba10a0c0107dd9cee3
muhezhiqiu@gmail.com <WangNan@git.com>
2024-06-04 18:38:10 +0800

    main

--- Thread/MyThread.java
+++ Thread/MyThread.java
@@ -1,2 +1,26 @@
-package Thread;public class MyThread {
+package Thread;
+
+public class MyThread extends Thread {
+
+    static int ticket;
+    static Object object = new Object();
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (MyThread.class) {
+                if (ticket < 100) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    ticket++;
+                    System.out.println(getName() + "购买了第" + ticket + "张票");
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
--- Thread/ThreadDemo.java
+++ Thread/ThreadDemo.java
@@ -1,2 +1,16 @@
-package Thread;public class ThreadDemo {
+package Thread;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.setName("t1");
+        MyThread t2 = new MyThread();
+        t2.setName("t2");
+        MyThread t3 = new MyThread();
+        t3.setName("t3");
+
+        t2.start();
+        t3.start();
+        t1.start();
+    }
 }
